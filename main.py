29f5da4d642e8a0a9fd13900a40c1a0fe62e935e
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-12-12 12:34:56 -0600

    login route finished

--- main.py
+++ main.py
@@ -90,9 +90,16 @@ def register():
 
 
 # TODO: Retrieve a user from the database based on their email.
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('get_all_posts'))
     return render_template("login.html", form=form)
 
 
